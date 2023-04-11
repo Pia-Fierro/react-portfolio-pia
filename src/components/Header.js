7d26f925e01e8b24bd9140e80f3da6a9c6aae430
Pia-Fierro <piafierro.3187@gmail.com>
2023-04-12 09:46:23 +1000

    testing antD for componets. Added dependencies.

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import { Layout, Space } from "antd";
+const { Header, Footer, Content } = Layout;
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
+};
+
+const Top = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: "100%",
+    }}
+    size={[0, 48]}
+  >
+      <Layout>
+        <Header style={headerStyle}>Pia Portfolio</Header>
+        {/* <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer> */}
+      </Layout>
+  </Space>
+);
+export default Top;
